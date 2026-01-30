80f3a832dc6ed05cbcf4fe91a8b06930765e71fe
Milan2006101 <fazekas.milan_2021d@bolyai-szakkozep.hu>
2026-01-30 15:30:49 +0100

    Clean main branch before importing backend

--- Backend/routok/authroutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { regisztracio , bejelentkezes} from "../controllerek/authcontroller";
-
-const router = Router();
-
-router.post("/regisztracio", regisztracio);
-router.post("/bejelentkezes", bejelentkezes); //az internet a post body-t ajadnlotta
-
-export default router;
